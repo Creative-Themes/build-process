d313e0e3c6519807a534f4fa54ea59ae733b2d67
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-12-20 11:21:55 +0200

    update autoprefixer settings

--- sass-task.js
+++ sass-task.js
@@ -79,7 +79,7 @@ function sassTask (gulp, options) {
                     options.sassIncludePaths
                 )
             }).on('error', sass.logError))
-            .pipe(autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+            .pipe(autoprefixer({browsers: ['last 2 versions']}))
             .pipe(gulpIf(entry.header, header(
                 (entry.header || {}).template,
                 (entry.header || {}).values
